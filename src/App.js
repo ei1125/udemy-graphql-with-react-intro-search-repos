2101d8be7d8cca0085df97b0a12f0680a70e9e34
ei1125 <i.dont.like.monday1125@gmail.com>
2020-07-22 17:39:57 +0900

    27-show-search-results

--- src/App.js
+++ src/App.js
@@ -30,7 +30,6 @@ class App extends Component{
 
   render() {
     const { query, first, last, before, after } = this.state
-    console.log({query})
     return (
       <ApolloProvider client={client}>
         <form>
@@ -49,7 +48,23 @@ class App extends Component{
               const repositoryCount = search.repositoryCount
               const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories'
               const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
-            return <h2>{title}</h2>
+              return (
+                <React.Fragment>
+                  <h2>{title}</h2>
+                  <ul>
+                    {
+                      search.edges.map(edge => {
+                        const node = edge.node
+                        return (
+                          <li key={node.id}>
+                            <a href={node.url} target="_blank">{node.name}</a>
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                </React.Fragment>
+              )
             }
           }
         </Query>
