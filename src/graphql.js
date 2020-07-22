5ac84b50ddfaf71a7995b7c51d3f00cc3c88c27a
ei1125 <i.dont.like.monday1125@gmail.com>
2020-07-23 00:30:08 +0900

    32-add-star

--- src/graphql.js
+++ src/graphql.js
@@ -27,7 +27,16 @@ export const SEARCH_REPOSITORIES = gql`
     }
   }
 `
-
+export const ADD_STAR = gql`
+  mutation addstar ($input: AddStarInput!) {
+    addStar(input: $input) {
+      starrable {
+        id
+        viewerHasStarred
+      } 
+    }
+  }
+`
 
 
 export const ME = gql`
